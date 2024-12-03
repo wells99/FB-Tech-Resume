12a2a3e1a73ca5a0af5ec225319499052c1f6890
visitante <visitante@ffb.local>
2024-12-03 20:54:36 -0300

    Creditos ao Farias Brito e professores  que Proporcionaram esse evento

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+      extend: {
+        colors: {
+           "terracota": '#64221D',
+           " mostarda": "#F7AE1A",
+           "preto-medio": "#292625",
+           "marrom-escuro": "#3B2416",
+           "marrom-claro": "#A97744",
+           "nude": "#F5E7DA",
+           "marrom-branco":"#C7794A",
+           "marrom-preto": "#3B2416",
+           "cinza": "#C4C4C4",
+           "nu": "#F5E7DA",
+           "marrom-cinza": "#F5E7DA",
+          },
+      },
+    },
+    plugins: [],
+  }
